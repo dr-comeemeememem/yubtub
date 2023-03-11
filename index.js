04ae81f6dfea285e923ba4abab616ccbb6d66e5b
Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>
2023-03-11 10:41:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const axios = require('axios')
 const http = require('http')
 
+console.info("Server Ready!")
 var b;
 function a(a) {b=a}
 
 var yes;
 var server = http.createServer(function (url, abc) {
-  url.toString().split("https://")[1].split("/")
+  console.warn("Connection made!!!")
+  url.toString().split("localhost:443")[1].split("/")
   yes[0] = "https://youtube.com"
   axios.get(yes.join("/"))
-    .then(return => a(return))
-    .catch(err => console.log(err));
+    .then(def => a(def))
+    .catch(err => console.error(err));
   abc.writeHead(200, { 'Content-Type': JSON.parse(JSON.stringify(b.header).replace("-", "_")).content_type });
   if(new Set(JSON.parse(JSON.stringify(b.header).replace("-", "_")).content_type.split("")).has("text/") || new Set(JSON.parse(JSON.stringify(b.header).replace("-", "_")).content_type.split("")).has("application/"));
   abc.write(b.data.replace("https://youtube.com", "youtube.altk.xyz"));
@@ -18,4 +20,5 @@ var server = http.createServer(function (url, abc) {
 });
 
 server.listen(443); // https
-server.listen(80); // http
+//server.listen(80); // http
+console.info("Listening on 443")
